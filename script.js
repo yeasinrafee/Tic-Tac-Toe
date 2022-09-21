9e8dd2dea2292e36c77f6565918427b345fc3f73
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-22 02:08:47 +0600

    Module pattern added for game, game logic and changin the player's turn function added

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+const cells = document.querySelectorAll(".cell");
+const Game = (function () {
+  const cellArray = [];
+  cells.forEach((cell) => cellArray.push(cell));
+  let turn = "X";
+
+  //Function for changing turn:
+  const changeTurn = () => {
+    return turn === "X" ? "O" : "X";
+  };
+
+  //Function for checking who win:
+  const checkWinner = () => {};
+
+  //Game logic:
+  cellArray.forEach((element) => {
+    element.addEventListener("click", () => {
+      if (element.innerText === "") {
+        element.innerText = turn;
+        turn = changeTurn();
+        document.querySelector(".text").innerText = `Player ${turn}'s Turn`;
+      }
+    });
+  });
+
+  console.log(cellArray[0].getAttribute("data-set"));
+  console.log(cellArray[0].innerText);
+})();
