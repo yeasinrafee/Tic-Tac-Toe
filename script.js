0f996b5430f0109c5dbbe54a35db1dc105727908
yeasinrafee <mdyeasinrafi7@gmail.com>
2022-09-22 02:29:18 +0600

    function for checking winner is added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const Game = (function () {
   const cellArray = [];
   cells.forEach((cell) => cellArray.push(cell));
   let turn = "X";
+  let gameOver = false;
 
   //Function for changing turn:
   const changeTurn = () => {
@@ -11,7 +12,30 @@ const Game = (function () {
   };
 
   //Function for checking who win:
-  const checkWinner = () => {};
+  const checkWinner = () => {
+    let wins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    wins.forEach((e) => {
+      if (
+        cellArray[e[0]].innerText === cellArray[e[1]].innerText &&
+        cellArray[e[2]].innerText === cellArray[e[1]].innerText &&
+        cellArray[e[0]].innerText !== ""
+      ) {
+        document.querySelector(".text").innerText = `Player ${
+          cellArray[e[0]].innerText
+        } Won!`;
+        gameOver = true;
+      }
+    });
+  };
 
   //Game logic:
   cellArray.forEach((element) => {
@@ -19,7 +43,10 @@ const Game = (function () {
       if (element.innerText === "") {
         element.innerText = turn;
         turn = changeTurn();
-        document.querySelector(".text").innerText = `Player ${turn}'s Turn`;
+        checkWinner();
+        if (!gameOver) {
+          document.querySelector(".text").innerText = `Player ${turn}'s Turn`;
+        }
       }
     });
   });
